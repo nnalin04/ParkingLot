31bf39dd49e932532a77c2773bbec0ba3a48f7a1
nnalin04 <nnalin04@gmail.com>
2020-09-09 22:25:35 +0530

    Added test to check if the driver can park the car

---
+++ src/test/java/parkinglot/ParkingLotTest.java
@@ -0,0 +1,14 @@
+package parkinglot;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ParkingLotTest {
+
+    @Test
+    public void givenAVehicle_WhenParked_ShouldReturnTrue() {
+        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        boolean isParked = parkingLotSystem.park(new Object());
+        Assert.assertTrue(isParked);
+    }
+}
